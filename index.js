4f7ce5b391a1f798d1c88e6e261f91a4a6b31c65
Caitlin Majewski <cmajewski@wesleyan.edu>
2020-12-15 03:57:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Write your code here!
 document.querySelector("main#main").remove();
 
-let header=document.createElement('h1#victory')
-let newHeader=document.appendChild(header)
+let header=document.createElement("h1")
